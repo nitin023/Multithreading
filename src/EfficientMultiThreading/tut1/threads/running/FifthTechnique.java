26a3f47c5f9cfea8ebc8315765f1a6c04ec0efd5
nitin023 <khulbe.nitin@gmail.com>
2020-08-23 23:53:37 +0530

    Fifth technique using anonmynous class by passing runnable interface details to Thread class constructor itself

---
+++ src/EfficientMultiThreading/tut1/threads/running/FifthTechnique.java
@@ -0,0 +1,23 @@
+package EfficientMultiThreading.tut1.threads.running;
+
+import java.util.concurrent.TimeUnit;
+
+public class FifthTechnique {
+
+    public static void main(String[] args) {
+        System.out.println("Main thread starts here ........");
+        Thread t = new Thread(() -> {
+            for (int i = 10; i >= 0; i--) {
+                System.out.println("TICK TICK - " + i);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+        System.out.println("Main thread ends here ........");
+    }
+}
+
